76da4c873b6edb7c0de265f2c2d2b366cae9840f
pedro <pedro.adoneto@gmail.com>
2023-08-13 16:20:25 -0300

    remove build

--- Code/make/build/CMakeFiles/main.dir/main.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/main.cpp.obj: \
- C:\Users\pedro\workspace\personal\mastering-embedded\Code\make\main.cpp \
- C:\Users\pedro\workspace\personal\mastering-embedded\Code\make\log.hpp
